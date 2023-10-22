ce7e1413b8bdd0b7dccb0de2965b44b205ec2168
miguel <mihai.vieru@isa.utm.md>
2023-10-22 14:08:13 +0300

    scanner added

--- src/main/java/com/project/ngit/NgitApplication.java
+++ src/main/java/com/project/ngit/NgitApplication.java
@@ -1,9 +1,21 @@
 package com.project.ngit;
 
+import java.util.Scanner;
+
 public class NgitApplication {
 
+	static boolean isRunning = true;
 	public static void main(String[] args) {
-		System.out.println("sss");
+		try (Scanner scanner = new Scanner(System.in);) {
+
+			while (isRunning) {
+				String input = scanner.nextLine();
+				System.out.println(input);
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }
