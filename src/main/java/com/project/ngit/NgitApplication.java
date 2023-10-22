71e99c539f44d71ca8b9c7493a95607a2e21cdb4
miguel <mihai.vieru@isa.utm.md>
2023-10-22 14:02:55 +0300

    initial commit

---
+++ src/main/java/com/project/ngit/NgitApplication.java
@@ -0,0 +1,9 @@
+package com.project.ngit;
+
+public class NgitApplication {
+
+	public static void main(String[] args) {
+		System.out.println("sss");
+	}
+
+}
---
+++ src/test/java/com/project/ngit/NgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ngit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
