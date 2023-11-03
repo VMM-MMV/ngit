1f0cd4c35d8005d9dd1849dbff7849082784b0aa
VMM-MMV <121231490+VMM-MMV@users.noreply.github.com>
2023-11-03 16:08:50 +0200

    trees can save trees now

--- src/main/java/com/project/ngit/DirectoryLister.java
+++ src/main/java/com/project/ngit/DirectoryLister.java
@@ -1,16 +1,15 @@
 package com.project.ngit;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 import static com.project.ngit.AddCommand.readExistingData;
 
 public class DirectoryLister {
     static Path ngitPath;
+    static Map<String, FileStatus> existingData;
+
     public static List<File> listDirectories(String rootDirectoryPath) {
         File rootDirectory = new File(rootDirectoryPath);
         if (!rootDirectory.isDirectory()) {
@@ -42,63 +41,70 @@ public class DirectoryLister {
     }
 
     public static void makeTrees(String repositoryPath) {
-        DirectoryLister.ngitPath = Path.of(repositoryPath, ".ngit");
-
-        Map<String, FileStatus> existingData = readExistingData(ngitPath.resolve("index/changes.ser"));
+        ngitPath = Path.of(repositoryPath, ".ngit");
+        existingData = readExistingData(ngitPath.resolve("index/changes.ser"));
         List<File> directories = listDirectories(repositoryPath);
-        
-        String lastTree = null;
+        Collections.reverse(directories);
+
+        String headTree = null;
         for (File directory : directories) {
-            lastTree = makeOneTree(directory, existingData);
+            headTree =  makeOneTree(directory);
         }
 
-        System.out.println(lastTree);
+        System.out.println(headTree);
     }
 
-    public static String makeOneTree(File directory, Map<String, FileStatus> existingData) {
+    public static String makeOneTree(File directory) {
         File[] files = directory.listFiles();
         if (files == null) {
             return null;
         }
 
         StringBuilder directoryContentsHash = new StringBuilder();
-
         List<TreeStatus> treeInfo = new ArrayList<>();
 
         for (File file : files) {
+            System.out.println(file);
             String filePath = file.getAbsolutePath();
             if (existingData.containsKey(filePath)) {
                 FileStatus fileStatus = existingData.get(filePath);
+                System.out.println(fileStatus);
                 treeInfo.add(new TreeStatus(fileStatus.name(), fileStatus.fileHash()));
-
-                directoryContentsHash.append(fileStatus.fileHash()); //TO DO change path to relative path not absolute
+                directoryContentsHash.append(fileStatus.fileHash());
             }
         }
 
-        if(directoryContentsHash.length() != 0) {
-            System.out.println(directoryContentsHash);
-            String shaOfDirectoryContents = SHA.computeSHA(String.valueOf(directoryContentsHash));
-            System.out.println(shaOfDirectoryContents);
-            addBlob(shaOfDirectoryContents, treeInfo);
+        if (directoryContentsHash.length() != 0) {
+            String shaOfDirectoryContents = SHA.computeSHA(directoryContentsHash.toString());
+            String treeObjectPath = addBlob(shaOfDirectoryContents, treeInfo);
+
+            long lastModifiedTime = directory.lastModified();
+            FileStatus directoryStatus = new FileStatus(directory.getName(), treeObjectPath, new Date(lastModifiedTime).toString());
+            existingData.put(directory.getAbsolutePath(), directoryStatus);
+
             return shaOfDirectoryContents;
         }
         return null;
     }
 
+
     private static String addBlob(String shaOfDirectory, List<TreeStatus> statusOfTree) {
         String folderSHA = shaOfDirectory.substring(0, 2);
         String fileSHA = shaOfDirectory.substring(2);
 
-        String filePath = ngitPath + "\\objects" + "\\" + folderSHA;
+        String filePath = ngitPath + File.separator + "objects" + File.separator + folderSHA;
+        File directory = new File(filePath);
 
-        NgitApplication.makeFile(fileSHA, filePath);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
 
+        String fullFilePath = filePath + File.separator + fileSHA;
         List<String> fileString = Collections.singletonList(statusOfTree.toString());
 
         AddCommand.writeToFile(fileSHA, filePath, fileString);
-        return filePath + "\\" + fileSHA;
+        return fullFilePath;
     }
-
 
     public static void main(String[] args) {
         makeTrees("C:\\Users\\Mihai Vieru\\ngit2");
