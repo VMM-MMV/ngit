50e2545c69ba87cf3f30ab5cf7c60a92ca4b2be8
VMM-MMV <mihai.vieru@isa.utm.md>
2023-11-11 14:18:25 +0200

    added java doc to BlobStatus

--- src/main/java/com/project/ngit/ObjectStatuses/BlobStatus.java
+++ src/main/java/com/project/ngit/ObjectStatuses/BlobStatus.java
@@ -2,5 +2,26 @@ package com.project.ngit.ObjectStatuses;
 
 import java.io.Serializable;
 
-public record BlobStatus(String name, String fileHash, String lastModifiedDate) implements Serializable {
+/**
+ * The BlobStatus record represents the status of a 'blob' object in the NGit version control system.
+ * A 'blob' typically corresponds to a file in the repository.
+ * This record stores metadata about a file, including its name, hash, and last modified date.
+ */
+public record BlobStatus(
+        /**
+         * The name of the file.
+         */
+        String name,
+
+        /**
+         * The SHA-2 hash of the file's contents, used for identifying the contents in the NGit system.
+         */
+        String fileHash,
+
+        /**
+         * The last modified date of the file, stored as a string.
+         */
+        String lastModifiedDate
+) implements Serializable {
+    // The record implicitly declares final fields, a constructor, and accessor methods for all the fields.
 }
