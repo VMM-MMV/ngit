6e86828a117a7890919929a210bdcf59e48f3572
VMM-MMV <mihai.vieru@isa.utm.md>
2023-11-11 12:05:15 +0200

    added java doc to add command

--- src/main/java/com/project/ngit/Commands/AddCommand.java
+++ src/main/java/com/project/ngit/Commands/AddCommand.java
@@ -11,17 +11,31 @@ import java.util.*;
 import java.util.stream.Stream;
 import java.util.zip.Deflater;
 
+/**
+ * The AddCommand class is responsible for adding files to the staging area in an NGit repository.
+ * It processes files by computing their SHA-2 hash and storing their status.
+ */
 public class AddCommand {
     private final Path ngitPath;
     private final String repositoryPath;
     private Map<String, BlobStatus> existingData;
 
+    /**
+     * Constructs an AddCommand for a given repository path.
+     *
+     * @param repositoryPath the path to the repository where the command will be executed
+     */
     public AddCommand(String repositoryPath) {
         this.repositoryPath = repositoryPath;
         this.ngitPath = Path.of(repositoryPath, ".ngit");
         this.existingData = new HashMap<>();
     }
 
+    /**
+     * Executes the add command using the provided argument to determine the scope of the operation, then save the contents to changes.ser in the index folder.
+     *
+     * @param argument the argument specifying which files to add; "." indicates all files
+     */
     public void execute(String argument) {
         existingData = NgitApplication.readExistingData(ngitPath.resolve("index/changes.ser"));
 
@@ -35,6 +49,9 @@ public class AddCommand {
         NgitApplication.saveDataToFile(ngitPath.resolve("index/changes.ser"), existingData);
     }
 
+    /**
+     * Processes all files in the repository for addition to the staging area.
+     */
     private void processAllFilesInRepository() {
         try (Stream<Path> stream = Files.walk(Path.of(repositoryPath))) {
             stream.forEach(this::processPath);
@@ -43,11 +60,21 @@ public class AddCommand {
         }
     }
 
+    /**
+     * Processes a single file for addition to the staging area.
+     *
+     * @param argument the relative path to the file from the repository root
+     */
     private void processSingleFile(String argument) {
         Path filePath = Path.of(repositoryPath).resolve(argument);
         processPath(filePath);
     }
 
+    /**
+     * Processes the given path and adds it to the staging area if it is a file.
+     *
+     * @param path the path to the file to be processed
+     */
     private void processPath(Path path) {
         if (Files.isDirectory(path) || String.valueOf(path).contains(".ngit") || String.valueOf(path).contains(".git")) {
             return;
@@ -67,6 +94,14 @@ public class AddCommand {
         existingData.put(path.toString(), blobStatus);
     }
 
+    /**
+     * Adds a blob to the NGit object store for the given file path.
+     *
+     * @param ngitPath the path to the NGit directory
+     * @param path     the path to the file to add as a blob
+     * @return the SHA-2 hash of the added file
+     * @throws IOException if an I/O error occurs
+     */
     public String addBlob(String ngitPath, String path) throws IOException {
         String shaOfFile = SHA.fileToSHA(path);
         String gitObjectDirectory = shaOfFile.substring(0, 2);
@@ -85,6 +120,13 @@ public class AddCommand {
         return shaOfFile;
     }
 
+    /**
+     * Compresses the given byte array using the Deflater algorithm. This encrypts the file contents for storage improvements.
+     *
+     * @param data the data to compress
+     * @return the compressed data as a byte array
+     * @throws IOException if an I/O error occurs during compression
+     */
     private byte[] compress(byte[] data) throws IOException {
         Deflater deflater = new Deflater();
         deflater.setInput(data);
