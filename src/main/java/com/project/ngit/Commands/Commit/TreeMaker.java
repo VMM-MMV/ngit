f2c8b2e66e820982d9bce70da5788bc3ee6ccaf1
VMM-MMV <mihai.vieru@isa.utm.md>
2023-11-11 10:19:17 +0200

    improved code in tree maker

--- src/main/java/com/project/ngit/Commands/Commit/TreeMaker.java
+++ src/main/java/com/project/ngit/Commands/Commit/TreeMaker.java
@@ -1,16 +1,18 @@
 package com.project.ngit.Commands.Commit;
 
-
-
 import com.project.ngit.Hash.SHA;
 import com.project.ngit.ObjectStatuses.BlobStatus;
 import com.project.ngit.ObjectStatuses.TreeStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 class TreeMaker {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TreeMaker.class);
     private final Path objectsPath;
     private String headTree;
     private final Map<String, BlobStatus> existingData;
@@ -25,74 +27,52 @@ class TreeMaker {
     }
 
     public void makeTrees(Path ngitPath) {
-        List<File> directories = listOfDirectories(ngitPath.getParent().toString());
+        List<Path> directories = listOfDirectories(ngitPath.getParent());
         Collections.reverse(directories);
 
-        for (File directory : directories) {
+        for (Path directory : directories) {
             makeOneTree(directory);
         }
 
         System.out.println(headTree);
     }
 
-    private List<File> listOfDirectories(String rootDirectoryPath) {
-        File rootDirectory = new File(rootDirectoryPath);
-        if (!rootDirectory.isDirectory()) {
-            throw new IllegalArgumentException("The provided file object is not a directory");
+    private List<Path> listOfDirectories(Path rootDirectoryPath) {
+        if (!Files.isDirectory(rootDirectoryPath)) {
+            throw new IllegalArgumentException("The provided path is not a directory");
         }
 
-        return breadthFirstDirectoryTraversal(rootDirectory);
+        try {
+            return Files.walk(rootDirectoryPath)
+                    .filter(Files::isDirectory)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("Error traversing directories", e);
+        }
     }
 
-    private List<File> breadthFirstDirectoryTraversal(File rootDirectory) {
-        Queue<File> queue = new LinkedList<>();
-        queue.offer(rootDirectory);
+    private void makeOneTree(Path directoryPath) {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directoryPath)) {
+            List<TreeStatus> treeInfo = collectTreeInfo(stream);
 
-        List<File> directories = new ArrayList<>();
-        directories.add(rootDirectory);
+            if (!treeInfo.isEmpty()) {
+                String shaOfDirectoryContents = computeDirectorySHA(treeInfo);
+                headTree = shaOfDirectoryContents; // It's value will change constantly and the last value here will be the head of all the trees
 
-        while (!queue.isEmpty()) {
-            File currentDir = queue.poll();
-            File[] files = currentDir.listFiles();
-
-            if (files == null) {
-                continue;
+                writeTree(shaOfDirectoryContents, treeInfo);
+                existingData.put(String.valueOf(directoryPath), new BlobStatus(directoryPath.getFileName().toString(), shaOfDirectoryContents, "tree"));
             }
-
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    directories.add(file);
-                    queue.offer(file);
-                }
-            }
-        }
-        return directories;
-    }
-
-    private void makeOneTree(File directory) {
-        File[] files = directory.listFiles();
-        if (files == null) {
-            return;
-        }
-
-        List<TreeStatus> treeInfo = collectTreeInfo(files);
-
-        if (!treeInfo.isEmpty()) {
-            String shaOfDirectoryContents = computeDirectorySHA(treeInfo);
-            headTree = shaOfDirectoryContents; // It's value will change constantly and the last value here will be the head of all the trees
-
-            writeTree(shaOfDirectoryContents, treeInfo);
-            existingData.put(directory.getAbsolutePath(), new BlobStatus(directory.getName(), shaOfDirectoryContents, "tree"));
+        } catch (IOException e) {
+            LOGGER.error("An error occurred while listing files in directory: {}", directoryPath, e);
         }
     }
 
-    private List<TreeStatus> collectTreeInfo(File[] files) {
+    private List<TreeStatus> collectTreeInfo(DirectoryStream<Path> directoryStream) {
         List<TreeStatus> treeInfo = new ArrayList<>();
-        for (File file : files) {
-            String filePath = file.getAbsolutePath();
-            if (existingData.containsKey(filePath)) {
-                BlobStatus fileStatus = existingData.get(filePath);
-                String objectType = file.isDirectory() ? "tree" : "blob";
+        for (Path path : directoryStream) {
+            if (existingData.containsKey(path)) {
+                BlobStatus fileStatus = existingData.get(path);
+                String objectType = Files.isDirectory(path) ? "tree" : "blob";
                 treeInfo.add(new TreeStatus(fileStatus.name(), fileStatus.fileHash(), objectType));
             }
         }
@@ -108,21 +88,16 @@ class TreeMaker {
     }
 
     protected void writeTree(String shaOfDirectoryContents, List<TreeStatus> treeInfo) {
-        String folderSHA = shaOfDirectoryContents.substring(0, 2);
-        String fileSHA = shaOfDirectoryContents.substring(2);
+        Path folderPath = objectsPath.resolve(shaOfDirectoryContents.substring(0, 2));
+        Path treeFilePath = folderPath.resolve(shaOfDirectoryContents.substring(2));
 
-        String filePath = objectsPath.resolve(folderSHA).toString();
-        File directory = new File(filePath);
-
-        if (!directory.exists() && !directory.mkdirs()) {
-            throw new IllegalStateException("Could not create directory: " + directory);
-        }
-
-        String fullFilePath = Paths.get(filePath, fileSHA).toString();
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fullFilePath))) {
-            oos.writeObject(treeInfo);
+        try {
+            Files.createDirectories(folderPath);
+            try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(treeFilePath))) {
+                oos.writeObject(treeInfo);
+            }
         } catch (IOException e) {
-            throw new RuntimeException("An error occurred while writing the tree to the file: " + fullFilePath, e);
+            LOGGER.error("An error occurred while writing the tree to the file: {}", treeFilePath, e);
         }
     }
 }
