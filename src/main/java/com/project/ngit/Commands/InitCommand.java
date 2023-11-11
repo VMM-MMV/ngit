9de129d2356ebc5425d562f20c92bb945cc9e1e0
VMM-MMV <mihai.vieru@isa.utm.md>
2023-11-11 12:23:35 +0200

    added java doc to init command

--- src/main/java/com/project/ngit/Commands/InitCommand.java
+++ src/main/java/com/project/ngit/Commands/InitCommand.java
@@ -2,17 +2,34 @@ package com.project.ngit.Commands;
 
 import com.project.ngit.NgitApplication;
 
+/**
+ * The InitCommand class is responsible for initializing a new NGit repository.
+ * This process involves creating the necessary directory structure for the repository to function.
+ */
 public class InitCommand {
     private final String repositoryPath;
 
+    /**
+     * Constructs an InitCommand with the specified repository path.
+     *
+     * @param repositoryPath the path where the new NGit repository will be initialized
+     */
     public InitCommand(String repositoryPath) {
         this.repositoryPath = repositoryPath;
     }
 
+    /**
+     * Executes the initialization of the NGit repository.
+     * It creates the .ngit directory along with subdirectories for objects, index, and heads.
+     */
     public void execute() {
+        // Create the main .ngit directory which will contain all the repository data
         NgitApplication.makeFolder(".ngit", repositoryPath);
+        // Create the objects directory to store all the git objects like commits, trees, and blobs
         NgitApplication.makeFolder(".ngit/objects", repositoryPath);
+        // Create the index directory to hold information about the current working directory state
         NgitApplication.makeFolder(".ngit/index", repositoryPath);
+        // Create the heads directory to keep track of branch pointers
         NgitApplication.makeFolder(".ngit/heads", repositoryPath);
     }
 }
