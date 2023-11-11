4bfcf93f81427c896f10684d176753a7769f140e
VMM-MMV <mihai.vieru@isa.utm.md>
2023-11-11 14:20:44 +0200

    added java doc to CommitStatus

--- src/main/java/com/project/ngit/ObjectStatuses/CommitStatus.java
+++ src/main/java/com/project/ngit/ObjectStatuses/CommitStatus.java
@@ -2,5 +2,36 @@ package com.project.ngit.ObjectStatuses;
 
 import java.io.Serializable;
 
-public record CommitStatus(String currentCommit, String previousCommit, String creator, String content, String message) implements Serializable {
+/**
+ * The CommitStatus record represents the status of a commit in the NGit version control system.
+ * It encapsulates the details of a commit, including references to the current and previous commits,
+ * the creator of the commit, the commit's content, and the commit message.
+ */
+public record CommitStatus(
+        /**
+         * The SHA-2 hash of the current commit.
+         */
+        String currentCommit,
+
+        /**
+         * The SHA-2 hash of the previous commit. This is used to form the commit chain in the version history.
+         */
+        String previousCommit,
+
+        /**
+         * The name or identifier of the creator of the commit.
+         */
+        String creator,
+
+        /**
+         * The content of the commit, which include references to the tree objects that represent the directory structure.
+         */
+        String content,
+
+        /**
+         * The message associated with the commit, providing a description of the changes or the purpose of the commit.
+         */
+        String message
+) implements Serializable {
+    // The record implicitly declares final fields, a constructor, and accessor methods for all the fields.
 }
